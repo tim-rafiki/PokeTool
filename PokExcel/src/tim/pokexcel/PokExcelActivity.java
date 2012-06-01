e0267cc700f86e172873db0e83b67dd93b8ed700
mibe <mibe@sfc.wide.ad.jp>
2012-06-01 18:04:27 +0900

    ログイン処理の変更 メニューからログイン処理を行うように変更した あと，ライブラリを監視対象に追加

--- PokExcel/src/tim/pokexcel/PokExcelActivity.java
+++ PokExcel/src/tim/pokexcel/PokExcelActivity.java
@@ -8,11 +8,12 @@ import com.dropbox.client2.exception.DropboxException;
 import com.dropbox.client2.session.Session.AccessType;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -55,12 +56,13 @@ public class PokExcelActivity extends Activity {
 	private boolean mLoggedIn;
 	
 	// インターフェイスのボタン
-	private Button mSubmit;
+	//private Button mSubmit;
 	
 	//////////////////////////
 	// ここからメソッド宣言 //
 	//////////////////////////
 	
+	// アクティビティ作成時に呼ばれるメソッド
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -70,6 +72,9 @@ public class PokExcelActivity extends Activity {
 		if (savedInstanceState != null) {
 			//mCameraFileName = savedInstanceState.getString("mCameraFileName");
 		}
+		
+		// ウィンドウ名を変更する
+		setTitle(R.string.window_name);
 		
 		// アプリの識別キーと認証パスを取得する
 		app_key = getString(R.string.app_key);
@@ -81,27 +86,13 @@ public class PokExcelActivity extends Activity {
 		
 		session = dbc.mApi.getSession();
 		
-		mSubmit = (Button)findViewById(R.id.auth_button);
-		
-		mSubmit.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				// This logs you out if you're logged in, or vice versa
-				if (mLoggedIn) {
-					dbc.logOut();
-					//logOut();
-					setLoggedIn(false);
-				} else {
-					// Start the remote authentication
-					dbc.startAuthentication();
-				}
-			}
-		});
-		
 		// Display the proper UI state if logged in or not
 		setLoggedIn(session.isLinked());
 		//*/
 	}
 	
+	// アクティビティのレジューム時に呼ばれるメソッド
+	// Dropbox認証アクティビティ終了後にも実行されるっぽい
 	@Override
 	protected void onResume() {
 		super.onResume();
@@ -112,7 +103,6 @@ public class PokExcelActivity extends Activity {
 			Log.d(TAG, "onResume: session = null");
 			return;
 		}
-		//AndroidAuthSession session = mApi.getSession();
 		
 		// The next part must be inserted in the onResume() method of the
 		// activity from which session.startAuthentication() was called, so
@@ -141,15 +131,123 @@ public class PokExcelActivity extends Activity {
 		
 		mLoggedIn = loggedIn;
 		if (loggedIn) {
-			mSubmit.setText("Unlink from Dropbox");
+			//mSubmit.setText("Unlink from Dropbox");
 			setTextView(textView);
 			textView.setVisibility(TextView.VISIBLE);
 		} else {
-			mSubmit.setText("Link with Dropbox");
+			//mSubmit.setText("Link with Dropbox");
 			textView.setVisibility(TextView.GONE);
 		}
 	}
 	
+	// メニューボタンを最初に押したときの処理
+	public boolean onCreateOptionsMenu(Menu menu){
+		super.onCreateOptionsMenu(menu);
+		Log.d(TAG, "onCreateOptionsMenu");
+		
+		getMenuInflater().inflate(R.menu.menu, menu);
+		
+		return true;
+	}
+	
+	// メニューボタンの内容を選択した時の処理
+	public boolean onOptionsItemSelected(MenuItem item){
+		int id = item.getItemId();
+		Log.d(TAG, "onOptionsItemSelected: id = " + id);
+		
+		// 項目のIDで分岐
+		switch(item.getItemId()) {
+		case R.id.menuItem_option:
+			// 設定ボタンが押された時
+			return onOptionSelected();
+		}
+		
+		// 未設定な場合はここ
+		return false;
+	}
+	
+	// オプションボタンが選択された時の処理
+	private boolean onOptionSelected(){
+		Log.d(TAG, "onOptionSelected");
+		
+		// 設定の一覧
+		CharSequence[] optionList = new CharSequence[]{
+				getString(R.string.dialog_config_loginDropbox),
+				getString(R.string.dialog_config_setPokExcel)
+		};
+		
+		// 既にPokExcelファイルが指定されているとき
+		if(mLoggedIn){
+			
+			// 設定テキストの変更
+			optionList[0] = getString(R.string.dialog_config_logoutDropbox);
+		}
+		
+		// 設定の番号
+		final int dialog_config_setDropbox =  0x0;
+		final int dialog_config_setPokExcel =  0x1;
+		
+		// オプションメニューを表示する
+		new AlertDialog.Builder(activity)
+		.setTitle(R.string.dialog_config_title)
+		.setItems(optionList, new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				
+				// 選択したボタンによって分岐する
+				switch(which){
+				case dialog_config_setDropbox:
+					// アカウント設定が選択されたとき
+					
+					// Dropboxログイン状態を設定する
+					setDropbox();
+					break;
+				case dialog_config_setPokExcel:
+					// ファイル指定が選択されたとき
+					
+					// データベース用のファイルを指定する
+					break;
+				}
+			}
+		})
+		.show();
+		
+		return false;
+	}
+	
+	// Dropboxのログイン状態を設定する
+	private boolean setDropbox(){
+		Log.d(TAG, "setDropbox");
+		
+		// 既にログインしている時
+		if(mLoggedIn){
+			
+			// ログアウトを確認するダイアログを表示する
+			new AlertDialog.Builder(activity)
+			.setTitle(R.string.dialog_logout_title)
+			.setPositiveButton(R.string.common_run, new DialogInterface.OnClickListener() {
+				
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					// ログアウトし，状態を未接続に変更する
+					dbc.logOut();
+					setLoggedIn(false);
+				}
+			})
+			.setNegativeButton(R.string.common_cancel, null)
+			.show();
+			
+			return false;
+		}
+		
+		// 認証を実行する（ログインアクティビティ起動）
+		dbc.startAuthentication();
+		
+		return true;
+	}
+	
+	// ファイルリストを作成する
 	private void setTextView(TextView textView) {
 		Log.d(TAG, "setTextView");
 		
@@ -159,7 +257,7 @@ public class PokExcelActivity extends Activity {
 			
 			String text = "";
 			for(final Entry entry : fileList){
-				text = text + ", " + entry.path;
+				text = text + ",\n" + entry.path;
 			}
 			
 			textView.setText(text);
@@ -170,7 +268,7 @@ public class PokExcelActivity extends Activity {
 		}
 		
 	}
-
+	
 	private void showToast(String msg) {
 		Log.d(TAG, "showToast");
 		
